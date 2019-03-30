d0fd0e6eb1e9228c52fabfdc0bcb8cd1598c203a
Timothy Spann <tspann@hortonworks.com>
2019-03-29 23:30:36 -0400

    Create spacy.py

---
+++ spacy.py
@@ -0,0 +1,9 @@
+import spacy
+
+# major upgrade
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(u"Cloudera is releasing new streaming technologies.   Apache NiFi 1.9.1 is one of them.")
+
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
